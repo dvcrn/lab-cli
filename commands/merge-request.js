6af214124c6bfd120f1f5a2375658ef7daf944ee
David Mohl <me@dave.cx>
2016-08-09 14:20:27 +0900

    feat(merge-request): allow omitting of source

--- commands/merge-request.js
+++ commands/merge-request.js
@@ -30,11 +30,18 @@ function createMr(project, base, from, title) {
 }
 
 function mergeRequest(base, from, title) {
-  if (arguments.length <= 1) {
+  if (arguments.length <= 0) {
     console.info("Not enough arguments");
     console.info("");
     help.printUsage();
     return;
+  }
+
+  // Case when ommitting both, base and from
+  if (arguments.length === 1) {
+    title = base;
+    from = null;
+    base = 'master';
   }
 
   // Allow to omit base branch and auto set it to master
@@ -51,13 +58,24 @@ function mergeRequest(base, from, title) {
     }
 
     git.getRepositoryFromGit(function(project) {
-      return createMr(project.toLowerCase(), base, from, title);
+      git.getBranchFromGit(function(branch) {
+        if (from === null) {
+          return createMr(project.toLowerCase(), base, branch, title);
+        }
+
+        return createMr(project.toLowerCase(), base, from, title);
+      });
     });
   });
 }
 
 module.exports = {
   fx: mergeRequest,
-  cmd: "lab mr|merge-request base_branch=master source_branch title",
-  desc: "Creates a new merge request from source_branch -> base_branch. Takes the project from git config."
+  cmd: "lab mr|merge-request base_branch=master source_branch=current_branch title",
+  desc: [
+    "Creates a new merge request from source_branch -> base_branch. Takes the project from git config.",
+    "if base_branch is ommited, it will default to master",
+    "if source_branch is ommited as well, lab-cli will extract the current branch from git",
+    "the shortest command would be: lab mr 'title'"
+  ]
 }
